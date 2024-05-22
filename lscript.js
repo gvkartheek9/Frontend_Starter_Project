a4bfee9e6bc3750da34633af42f5107428e7ac61
gvkartheek9 <gvkartheek2024@outlook.com>
2024-05-22 13:02:21 +0530

    updated the login form 22_may

---
+++ lscript.js
@@ -0,0 +1,40 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const form = document.querySelector('form');
+    
+    form.addEventListener('submit', function(event) {
+        const username = document.getElementById('username').value;
+        const password = document.getElementById('password').value;
+
+        const usernamePattern = /^[a-zA-Z0-9]+$/;
+
+        if (!username) {
+            alert('Please enter a username.');
+            event.preventDefault();
+            return;
+        }
+
+        if (!usernamePattern.test(username)) {
+            alert('Username should not contain special characters or spaces.');
+            event.preventDefault();
+            return;
+          }
+
+          const passwordPattern = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+
+
+        if (!password) {
+            alert('Please enter a password.');
+            event.preventDefault();
+            return;
+        }
+
+        if (!passwordPattern.test(password)) {
+            alert('Password must be at least 8 characters long, and include at least one uppercase letter, one lowercase letter, one number, and one special character.');
+            event.preventDefault();
+            return;
+          }
+
+        // If the validation is successful, the form will be submitted
+        // and the user will be redirected to landing.html as per the form's action attribute
+    });
+});
