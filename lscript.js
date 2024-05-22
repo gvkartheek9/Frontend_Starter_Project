31f10eddf49a7f84563fa371b791f6b03a56341f
gvkartheek9 <gvkartheek2024@outlook.com>
2024-05-22 14:02:35 +0530

    another updation

--- lscript.js
+++ lscript.js
@@ -33,8 +33,10 @@ document.addEventListener('DOMContentLoaded', function() {
             event.preventDefault();
             return;
           }
-
+          window.location.href = 'landing.html';
         // If the validation is successful, the form will be submitted
         // and the user will be redirected to landing.html as per the form's action attribute
+
+        event.preventDefault();
     });
 });
